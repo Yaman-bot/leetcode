b337bd23cb07dffe222c7238d2ae68860d54f7d3
Yaman Aggarwal <yaman2661@gmail.com>
2021-12-28 16:43:25 +0530

    Time: 12 ms (76.87%), Space: 10.9 MB (44.81%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        //Key is the number and value is its index in the vector.
-        unordered_map<int, int> m;
-        vector<int> result;
-        for (int i = 0; i < nums.size(); i++) {
-            int numberToFind = target - nums[i];
-
-            //if numberToFind is found in map, return them
-            if (m.find(numberToFind) != m.end()) {
-                result.push_back(m[numberToFind]);
-                result.push_back(i);			
-                return result;
+        vector<int> res;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            int rem=target-nums[i];
+            
+            if(m.find(rem)!=m.end()){
+                res.push_back(m[rem]);
+                res.push_back(i);
+                return res;
             }
-
-            //number was not found. Put it in the map.
-            m[nums[i]] = i;
+            
+            m[nums[i]]=i;
         }
-        return result;
+        return res;
     }
 };
