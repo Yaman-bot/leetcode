289200d5ca5dfaba00d1813c277976d5427b8105
Yaman Aggarwal <yaman2661@gmail.com>
2022-01-31 12:00:49 +0530

    Time: 43 ms (83.44%), Space: 9.2 MB (85.93%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-   bool canPartition(vector<int>& nums) {
+    bool canPartition(const vector<int> &nums)
+    {
+        int n = nums.size();
         int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < n; i++)
             sum += nums[i];
-        }
 
-        if (sum % 2 != 0) {
-          return false;
-        }
-        
-        sum=sum/2;
+        if (sum % 2 != 0)
+            return false;
 
-        vector<vector<bool>> dp(nums.size(), vector<bool>(sum + 1));
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
-        
-        for(int s=1;s<=sum;s++){
-            dp[0][s]=(nums[0]==s ? true:false);
-        }
-        
-        for (int i = 1; i < n; i++)
-        {
-            for (int s = 1; s <= sum; s++)
-            {
-                // if we can get the sum 's' without the number at index 'i'
-                if (dp[i - 1][s])
-                    dp[i][s] = dp[i - 1][s];
-                // else if we can find a subset to get the remaining sum
-                else if (s >= nums[i])
-                    dp[i][s] = dp[i - 1][s - nums[i]];
-            }
-        }
-        
-        return dp[n-1][sum];
+        sum /= 2;
+        vector<bool> dp(sum + 1);
+        dp[0] = true;
+
+        for (auto num : nums)
+            for (int s = sum; s >= num; s--)
+                if (dp[s - num] || s == num)
+                    dp[s] = true;
+
+        return dp[sum];
     }
 };
