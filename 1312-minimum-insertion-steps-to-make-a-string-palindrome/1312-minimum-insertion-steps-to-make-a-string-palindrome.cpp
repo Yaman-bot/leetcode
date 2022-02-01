68ca6385ca5556666d983f4acfa7c1f0e4a76574
Yaman Aggarwal <yaman2661@gmail.com>
2022-02-01 23:35:33 +0530

    Time: 67 ms (32.41%), Space: 27.2 MB (13.53%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+        int longestPalindromeSubseq(string s)
+    {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++)
+            dp[i][i] = 1;
+
+        for (int start = n; start >= 0; start--)
+        {
+            for (int end = start + 1; end < n; end++)
+            {
+                if (s[start] == s[end])
+                    dp[start][end] = dp[start + 1][end - 1] + 2;
+                else
+                    dp[start][end] = max(dp[start + 1][end], dp[start][end - 1]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+    
+    int minInsertions(string s) {
+        int n = s.length();
+        int lps = longestPalindromeSubseq(s);
+        return n - lps;
+    }
+};
