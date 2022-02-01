e7e3afeca17ebcb7def25e5ac0f47dcb42e8d2c2
Yaman Aggarwal <yaman2661@gmail.com>
2022-02-01 23:16:30 +0530

    Time: 40 ms (24.34%), Space: 7.7 MB (41.66%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int ans=0;
+        for (int i = 0; i < n; i++)
+            dp[i][i] = true;
+
+        for (int start = n; start >= 0; start--)
+        {
+            for (int end = start + 1; end < n; end++)
+            {
+                if (s[start] == s[end])
+                {
+                    if (end - start == 1 || dp[start + 1][end - 1] == true)
+                        dp[start][end] = true;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dp[i][j])
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
