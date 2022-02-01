ed6e6b6029a48ceb90d22b8d313806c96e7232c3
Yaman Aggarwal <yaman2661@gmail.com>
2022-02-01 23:18:43 +0530

    Time: 29 ms (31.08%), Space: 7.7 MB (46.92%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -4,27 +4,26 @@ public:
         int n = s.length();
         vector<vector<bool>> dp(n, vector<bool>(n, false));
         int ans=0;
-        for (int i = 0; i < n; i++)
+        
+        for (int i = 0; i < n; i++){
             dp[i][i] = true;
-
+            ans++;
+        }
+        
         for (int start = n; start >= 0; start--)
         {
             for (int end = start + 1; end < n; end++)
             {
                 if (s[start] == s[end])
                 {
-                    if (end - start == 1 || dp[start + 1][end - 1] == true)
+                    if (end - start == 1 || dp[start + 1][end - 1] == true){
                         dp[start][end] = true;
+                        ans++;
+                    }
                 }
             }
         }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(dp[i][j])
-                    ans++;
-            }
-        }
         return ans;
     }
 };
