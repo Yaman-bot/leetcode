ff675602664b71c105e350b8f60ea03e2595e35b
Yaman Aggarwal <yaman2661@gmail.com>
2022-02-02 16:59:06 +0530

    Time: 64 ms (65.85%), Space: 8.2 MB (72.57%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    int minCut(string s)
+    {
+        int n = s.length();
+        vector<vector<bool>> dpPalindrome(n, vector<bool>(n, 0));
+
+        for (int i = 0; i < n; i++)
+            dpPalindrome[i][i] = true;
+
+        for (int start = n - 1; start >= 0; start--)
+        {
+            for (int end = start + 1; end < n; end++)
+            {
+                if (s[start] == s[end])
+                {
+                    if (end - start == 1 || dpPalindrome[start + 1][end - 1])
+                        dpPalindrome[start][end] = true;
+                }
+            }
+        }
+
+        vector<int> cuts(n, 0);
+        for (int start = n - 1; start >= 0; start--)
+        {
+            int minCuts = n;
+            for (int end = n - 1; end >= start; end--)
+            {
+                if (dpPalindrome[start][end])
+                {
+                    minCuts = (end == n - 1) ? 0 : min(minCuts, cuts[end + 1] + 1);
+                }
+            }
+            cuts[start] = minCuts;
+        }
+        return cuts[0];
+    }
+};
