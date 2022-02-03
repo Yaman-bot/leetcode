a6309f4cf4cc85522e129fd4e64b5f2fb4e7ce3f
Yaman Aggarwal <yaman2661@gmail.com>
2022-02-03 22:27:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.46%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double solve(double x, int n){
+            if(n == 0)
+                return 1;
+
+            if(n == 1)
+                return x;
+            
+            double ans = solve(x, n/2);
+            if(n%2 == 0)
+                return ans * ans;
+            else
+                return x * ans * ans;
+    }
+    double myPow(double x, int n) {
+        if(n < 0)
+            return 1 / solve(x, n);
+        else
+            return solve(x, n);
+    }
+};
