bcd925d9df7272aa7a7153658d15e1c9e0baa802
Yaman Aggarwal <yaman2661@gmail.com>
2022-02-03 22:27:29 +0530

    Time: 6 ms (9.02%), Space: 5.9 MB (80.92%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -4,9 +4,6 @@ public:
             if(n == 0)
                 return 1;
 
-            if(n == 1)
-                return x;
-            
             double ans = solve(x, n/2);
             if(n%2 == 0)
                 return ans * ans;
