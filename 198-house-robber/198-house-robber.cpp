fdf7a6cd9620103dbb25846db99c4b0b705bcf4c
Yaman Aggarwal <yaman2661@gmail.com>
2022-03-01 12:00:08 +0530

    Time: 3 ms (53.51%), Space: 7.7 MB (76.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,14 +3,20 @@ public:
     int rob(vector<int>& nums) {
         if (nums.size() == 0)
             return 0;
+        
+        if(nums.size()==1) return nums[0];
+        if(nums.size()==2) return max(nums[0],nums[1]);
 
-        int prev1 = 0;
+        int prev1 = nums[0];
         int prev2 = 0;
-        for (int num : nums)
-        {
-            int tmp = prev1;
-            prev1 = max(prev2 + num, prev1);
-            prev2 = tmp;
+        for(int i=1;i<nums.size();i++){
+            int pick=nums[i];
+            if(i>1)
+                pick+=prev2;
+            int notPick=prev1;
+            int curr=max(pick,notPick);
+            prev2=prev1;
+            prev1=curr;
         }
         return prev1;
     }
