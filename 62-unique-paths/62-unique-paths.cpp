b9af50caac828dc5080d42fafd115782cedaeaa9
Yaman Aggarwal <yaman2661@gmail.com>
2022-03-02 22:38:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (54.16%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<vector<int>> &dp){
-        if(i==0 && j==0)
-            return 1;
-        if(i<0 || j<0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        int up=helper(i-1,j,dp);
-        int left=helper(i,j-1,dp);
-        dp[i][j]=up+left;
-        return dp[i][j];
-    }
-
     int uniquePaths(int m, int n)
     {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return helper(m - 1, n - 1, dp);
+        vector<int> dp(n, 0);
+        for (int i = 0; i < m; i++)
+        {
+            vector<int> temp(n, 0);
+            for (int j = 0; j < n; j++)
+            {
+                if(i==0 && j==0) 
+                    temp[j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0)
+                        //above row
+                        up=dp[j];
+                    if(j>0)
+                        //left column
+                        left=temp[j-1];
+                        //fill the current column
+                    temp[j]=up+left;
+                }
+            }
+            dp=temp;
+        }
+        return dp[n - 1];
     }
 };
