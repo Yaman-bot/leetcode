bc2b4a2630d3ad26f02100fd3ef4743872fa0e85
Yaman Aggarwal <yaman2661@gmail.com>
2022-03-03 13:15:10 +0530

    Time: 17 ms (9.86%), Space: 8.9 MB (34.01%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,27 @@
-class Solution {
+// Space Optimization-O(N)
+// Time-O(N*N)
+class Solution
+{
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-                int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        int n = triangle.size();
+        vector<int> dp(n, 0);
 
-        for(int j=0;j<n;j++)
-            dp[n-1][j]=triangle[n-1][j];
+        for (int j = 0; j < n; j++)
+            dp[j] = triangle[n - 1][j];
 
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int down=triangle[i][j]+dp[i+1][j];
-                int diagonal=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,diagonal);
+        for (int i = n - 2; i >= 0; i--)
+        {
+            vector<int> temp(n, 0);
+            for (int j = i; j >= 0; j--)
+            {
+                int down = triangle[i][j] + dp[j];
+                int diagonal = triangle[i][j] + dp[j + 1];
+                temp[j] = min(down, diagonal);
             }
+            dp = temp;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
