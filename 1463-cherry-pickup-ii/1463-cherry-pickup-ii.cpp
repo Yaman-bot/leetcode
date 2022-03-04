6de684c4b308adeb2904617fe8bc9b124edbb768
Yaman Aggarwal <yaman2661@gmail.com>
2022-03-04 13:32:16 +0530

    Time: 216 ms (10.01%), Space: 14.3 MB (44.79%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,25 +1,27 @@
-// Bottom Up-O(N*M*M)
+// Space OPtimized-O(N*M)
+// Time-O(N*M*M)
 class Solution
 {
-    public:
+public:
     int cherryPickup(vector<vector<int>> &grid)
     {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        vector<vector<int>> dp(m, vector<int>(m, 0));
         for (int j1 = 0; j1 < m; j1++)
         {
             for (int j2 = 0; j2 < m; j2++)
             {
                 if (j1 == j2)
-                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                    dp[j1][j2] = grid[n - 1][j1];
                 else
-                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+                    dp[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
 
         for (int i = n - 2; i >= 0; i--)
         {
+            vector<vector<int>> temp(m, vector<int>(m, 0));
             for (int j1 = 0; j1 < m; j1++)
             {
                 for (int j2 = 0; j2 < m; j2++)
@@ -35,16 +37,17 @@ class Solution
                             else
                                 val = grid[i][j1] + grid[i][j2];
                             if (j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m)
-                                val += dp[i + 1][j1 + dj1][j2 + dj2];
+                                val += dp[j1 + dj1][j2 + dj2];
                             else
                                 val += -1e9;
                             maxi = max(maxi, val);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    temp[j1][j2] = maxi;
                 }
             }
+            dp = temp;
         }
-        return dp[0][0][m - 1];
+        return dp[0][m - 1];
     }
 };
