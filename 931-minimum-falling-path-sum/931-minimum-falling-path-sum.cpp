054af00a3a7f73aabc172396c1881deb2d7af876
Yaman Aggarwal <yaman2661@gmail.com>
2022-03-04 11:09:43 +0530

    Time: 27 ms (25.58%), Space: 10.4 MB (20.21%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,33 +1,37 @@
-//Top Down-O(N*M)
+// Bottom Up-O(N*M)
 class Solution
 {
 public:
-    int f(int i, int j, int n,int m, vector<vector<int>> &matrix, vector<vector<int>> &dp)
-    {
-        if(j<0 || j>=m)
-            return 1e9;
-        if(i==0)
-            return matrix[0][j];
-        if(dp[i][j]!=-1)
-            return dp[i][j];    
-
-        int up=matrix[i][j]+f(i-1,j,n,m,matrix,dp);
-        int leftDiagonal=matrix[i][j]+f(i-1,j-1,n,m,matrix,dp);
-        int rightDiagonal=matrix[i][j]+f(i-1,j+1,n,m,matrix,dp);
-        dp[i][j]=min(up,min(leftDiagonal,rightDiagonal));
-        return dp[i][j];
-    }
     int minFallingPathSum(vector<vector<int>> &matrix)
     {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        int ans=INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
         for (int j = 0; j < m; j++)
+            dp[0][j] = matrix[0][j];
+
+        for (int i = 1; i < n; i++)
         {
-            int mini=f(n - 1, j, n,m, matrix, dp);
-            ans=min(ans,mini);
+            for (int j = 0; j < m; j++)
+            {
+                int up = dp[i - 1][j] + matrix[i][j];
+                int leftDiagonal = matrix[i][j];
+                if (j - 1 >= 0)
+                    leftDiagonal += dp[i - 1][j - 1];
+                else
+                    leftDiagonal += 1e9;
+                int rightDiagonal = matrix[i][j];
+                if (j + 1 < m)
+                    rightDiagonal += dp[i - 1][j + 1];
+                else
+                    rightDiagonal += 1e9;
+                dp[i][j] = min(up, min(leftDiagonal, rightDiagonal));
+            }
         }
+        int ans = 1e9;
+        for (int j = 0; j < m; j++)
+            ans = min(ans, dp[n - 1][j]);
         return ans;
     }
 };
