30a7c2c44a779749682a3d7c45133d0864283a71
Yaman Aggarwal <yaman2661@gmail.com>
2024-05-26 23:55:16 +0530

    Time: 19 ms (78.59%), Space: 7.5 MB (99.60%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,21 +6,23 @@ public:
         int n = text1.length();
         int m = text2.length();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        for(int i=0;i<=n;i++)
-            dp[i][0] = 0;
-        for(int j=0;j<=m;j++)
-            dp[0][j] = 0;
+        vector<int> prev(m + 1, 0),curr(m+1,0);
 
-        for (int i = 1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(text1[i-1]==text2[j-1])    //As in the string index will be at idx-1
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+        for (int j = 0; j <= m; j++)
+            prev[j] = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (text1[i - 1] == text2[j - 1]) // As in the string index will be at idx-1
+                    curr[j] = 1 + prev[j - 1];
                 else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    curr[j] = max(prev[j], curr[j - 1]);
             }
+            prev=curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
