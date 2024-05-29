71d04deee142256e8fde8a101506fab13f89a0bb
Yaman Aggarwal <yaman2661@gmail.com>
2024-05-29 16:55:39 +0530

    Time: 4 ms (82.87%), Space: 9.2 MB (98.55%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,25 +5,25 @@ public:
     {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        
-                //For j==0
-        for (int i = 0;i<=n;i++)
-            dp[i][0]=i;
+        vector<int> prev(m + 1, 0),curr(m+1,0);
 
-        //For i==0
-        for (int j = 0; j <= m;j++)
-            dp[0][j] = j;
+        // For i==0
+        for (int j = 0; j <= m; j++)
+            prev[j] = j;
 
-        for (int i = 1; i <= n;i++){
-            for(int j = 1; j <= m; j++){
-                if (word1[i-1] == word2[j-1])
-                    dp[i][j] = dp[i - 1][j - 1];
+        for (int i = 1; i <= n; i++)
+        {
+            curr[0] = i;
+            for (int j = 1; j <= m; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                    curr[j] = prev[j - 1];
                 else
-                    dp[i][j] = 1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                    curr[j] = 1 + min(prev[j], min(curr[j - 1], prev[j - 1]));
             }
+            prev=curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
