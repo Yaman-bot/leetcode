dd08eab72398f80f0c425087dd6f202ae1f9e8a0
Yaman Aggarwal <yaman2661@gmail.com>
2024-06-02 19:55:13 +0530

    Time: 134 ms (67.49%), Space: 77.9 MB (82.29%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,22 +4,10 @@ public:
     int maxProfit(vector<int> &prices)
     {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<int>> ahead(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
 
-        // When cap==0
-        for(int i = 0; i < n; i++){
-            for (int buy = 0; buy <= 1;buy++){
-                dp[i][buy][0] = 0;
-            }
-        }
-
-        //When ind==n
-        for(int buy = 0; buy <= 1; buy++){
-            for (int cap = 0; cap <= 2;cap++){
-                dp[n][buy][cap] = 0;
-            }
-        }
-        for (int i = n-1; i>=0; i--)
+        for (int i = n - 1; i >= 0; i--)
         {
             for (int buy = 0; buy <= 1; buy++)
             {
@@ -28,18 +16,19 @@ public:
                     int profit = 0;
                     if (buy)
                     { // Take                                  //Not take
-                        profit = max(-prices[i] + dp[i + 1][0][cap], 0 + dp[i + 1][1][cap]);
+                        profit = max(-prices[i] + ahead[0][cap], 0 + ahead[1][cap]);
                     }
                     else // Sell
                     {    // Take                                  //Not take
-                        profit = max(prices[i] + dp[i + 1][1][cap - 1], 0 + dp[i + 1][0][cap]);
+                        profit = max(prices[i] + ahead[1][cap - 1], 0 + ahead[0][cap]);
                     }
 
-                    dp[i][buy][cap] = profit;
+                    curr[buy][cap] = profit;
                 }
             }
+            ahead = curr;
         }
 
-        return dp[0][1][2];
+        return ahead[1][2];
     }
 };
