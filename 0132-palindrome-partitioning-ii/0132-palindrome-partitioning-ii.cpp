af17bbdcffed141e3d2e3f4e43a7621f6b70a32c
Yaman Aggarwal <yaman2661@gmail.com>
2024-06-11 12:49:11 +0530

    Time: 1499 ms (43.28%), Space: 7.8 MB (72.54%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,38 +1,36 @@
 class Solution
 {
 public:
-    bool isPalindrome(int i,int j,string &s){
-        while(i<j){
-            if(s[i]!=s[j])
+    bool isPalindrome(int i, int j, string &s)
+    {
+        while (i < j)
+        {
+            if (s[i] != s[j])
                 return false;
             i++;
             j--;
         }
         return true;
     }
-    int f(int i, int n, vector<int> &dp, string &s)
-    {
-        if(i==n)
-            return 0;
 
-        if(dp[i]!=-1)
-            return dp[i];
-
-        int mini = 1e9;
-        for (int j = i;j<n;j++){
-            if(isPalindrome(i,j,s)){
-                int cost=1+f(j+1,n,dp,s);
-                mini = min(mini,cost);
-            }
-        }
-        dp[i] = mini;
-        return dp[i];
-    }
     int minCut(string s)
     {
         int n = s.size();
-        vector<int> dp(n, -1);
-        //Subtracting 1 because of the extra partition at the end of string
-        return f(0, n, dp,s)-1;
+        vector<int> dp(n+1, 0);
+
+        for (int i = n - 1; i >= 0;i--){
+            int mini = 1e9;
+            for (int j = i; j < n; j++)
+            {
+                if (isPalindrome(i, j, s))
+                {
+                    int cost = 1 + dp[j + 1];
+                    mini = min(mini, cost);
+                }
+            }
+            dp[i] = mini;
+        }
+        // Subtracting 1 because of the extra partition at the end of string
+        return dp[0] - 1;
     }
 };
