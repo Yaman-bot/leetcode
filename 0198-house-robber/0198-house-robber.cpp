b7ef2c12e0bbdb46c06d301e3e5d65a55d2962e3
Yaman Aggarwal <yaman2661@gmail.com>
2024-06-20 08:56:00 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (10.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -17,7 +17,14 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return f(n-1,dp,nums);
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int pick=nums[i]+f(i-2,dp,nums);
+            int notPick=f(i-1,dp,nums);
+            dp[i]=max(pick,notPick);
+        }
+        
+        return dp[n-1];
     }
 };
